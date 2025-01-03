e4e0dac0963007869f75b85a14b1432368afddfb
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-01-03 19:02:25 +0330

    [ADD] array-numbers-count

---
+++ hello-typescript/src/array-numbers-count.ts
@@ -0,0 +1,13 @@
+function numbersCount<T>(inputArray: T[]): number {
+  let count = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (typeof inputArray[i] === "number") {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(numbersCount([1, 2, 3])); //3
+console.log(numbersCount([1, "2", 3])); //2
+console.log(numbersCount([1, "2", { id: 1, name: "a" }])); //1
