64ef93f443066a2816c4db6a643525d94031e29b
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-01-04 19:12:04 +0330

    [UPDATE] array-numbers-count

--- hello-typescript/src/array-numbers-count.ts
+++ hello-typescript/src/array-numbers-count.ts
@@ -1,4 +1,4 @@
-function numbersCount<T>(inputArray: T[]): number {
+function numbersCount(inputArray: unknown[]): number {
   let count = 0;
   for (let i = 0; i < inputArray.length; i++) {
     if (typeof inputArray[i] === "number") {
