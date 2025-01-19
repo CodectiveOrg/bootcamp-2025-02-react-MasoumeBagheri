1ded9a74e4f7c7ba132aecb2cefacf3d2e282f20
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-01-19 11:57:38 +0330

    [ADD] toolbar styles

--- dream-journal/src/components/toolbar/toolbar.tsx
+++ dream-journal/src/components/toolbar/toolbar.tsx
@@ -3,9 +3,11 @@ import { DreamFilter } from "../dream-filter";
 import { IconLight } from "../icons/icons";
 import { SearchBox } from "../search-box";
 
+import styles from "./toolbar.module.css";
+
 export const Toolbar: React.FC = () => {
   return (
-    <div className="toolbar">
+    <div className={styles.toolbar}>
       <SearchBox />
       <DreamFilter />
       <Button className="theme">
