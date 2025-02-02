1b77ec51c89bdde90f16c61e837544afa2bbe37d
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-02-02 11:23:28 +0330

    [REMOVE] divider from last dream

--- dream-journal/src/components/footer/footer.tsx
+++ dream-journal/src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../button";
 
 import { IconAdd } from "../icons/icons";
+import { Input } from "../input";
 
 import styles from "./footer.module.css";
 
@@ -15,6 +16,16 @@ export const Footer: React.FC = () => {
       >
         <IconAdd fill="var(--color-surface)" />
       </Button>
+      <dialog open>
+        <div className={styles.modal}>
+          <div className="title">New Dream</div>
+          <Input placeholder="Input your dream..." />
+          <div className="actions">
+            <Button>CANCEL</Button>
+            <Button>APPLY</Button>
+          </div>
+        </div>
+      </dialog>
     </footer>
   );
 };
