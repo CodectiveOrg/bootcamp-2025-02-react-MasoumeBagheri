de975797e879b44a53590bd31b555b7c6c42759b
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-02-05 22:05:16 +0330

    [UPDATE] textbox type

--- dream-journal/src/components/textbox/textboxtypes.ts
+++ dream-journal/src/components/textbox/textboxtypes.ts
@@ -1,9 +1,6 @@
 import { InputHTMLAttributes, ReactNode } from "react";
 
-export type Variant = "surface" | "primary" | "text" | "solid";
-
 export type TextBoxProps = InputHTMLAttributes<HTMLInputElement> & {
   className?: string;
-  variant?: Variant;
   suffixIcon?: ReactNode;
 };
