0d7964e4c58b75754ca62b6ea663dd0be8c0a437
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-02-06 00:29:46 +0330

    [DELETE] types folder

--- dream-journal/src/components/types/variant.type.ts
+++
@@ -1,7 +0,0 @@
-export type Variant = "surface" | "primary" | "text" | "solid";
-
-// const variantStyles: Record<Variant, string> = {
-//     surface: "var(--color-surface)",
-//     primary: "var(--color-primary)",
-//     text: "var(--color-text)",
-//   };
