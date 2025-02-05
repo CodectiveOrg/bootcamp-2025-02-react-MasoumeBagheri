d26b010eca6d68ca3728e14c442b2c8d76b01cc5
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-02-05 19:53:18 +0330

    [Delete icons use gulp]

--- dream-journal/src/components/icons/icons.ts
+++
@@ -1,8 +0,0 @@
-/* GENERATED FILE */
-
-export { default as IconAdd } from './src/add';
-export { default as IconDark } from './src/dark';
-export { default as IconEdit } from './src/edit';
-export { default as IconLight } from './src/light';
-export { default as IconSearch } from './src/search';
-export { default as IconTrash } from './src/trash';
