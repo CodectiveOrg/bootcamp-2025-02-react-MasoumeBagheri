ee395692c4cba750dcd816395850a9c6afb5eb33
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-02-06 14:43:42 +0330

    [UPDATE] button component

--- dream-journal/src/components/button/button.tsx
+++ dream-journal/src/components/button/button.tsx
@@ -1,8 +1,21 @@
-import styles from "./button.module.css";
-import { ButtonProps } from "./buttontypes";
+import { ButtonHTMLAttributes } from "react";
+
 import clsx from "clsx";
 
-export const Button: React.FC<ButtonProps> = ({
+import styles from "./button.module.css";
+
+type Variant = "solid" | "outlined" | "transparent";
+type Shape = "rectangle" | "circle" | "square";
+type Size = "small" | "medium" | "large";
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  className?: string;
+  variant?: Variant;
+  shape?: Shape;
+  size?: Size;
+};
+
+export const Button: React.FC<Props> = ({
   variant = "solid",
   shape = "rectangle",
   size = "small",
