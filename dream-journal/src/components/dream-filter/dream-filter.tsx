64ccc35fe63d93d0c6b6e70f662f33b937184fd7
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-02-21 15:04:52 +0330

    [DELETE] Dream Filter Component

--- dream-journal/src/components/dream-filter/dream-filter.tsx
+++
@@ -1,9 +0,0 @@
-export const DreamFilter: React.FC = () => {
-  return (
-    <select>
-      <option value="all">All</option>
-      <option value="good">Good</option>
-      <option value="bad">Bad</option>
-    </select>
-  );
-};
