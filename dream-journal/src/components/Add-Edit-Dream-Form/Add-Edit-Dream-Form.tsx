aaa7fcbd8ca9708b4c7af4c21923666df69a8831
MasoumeBagheri <masoome.bagheri@gmail.com>
2025-03-07 10:21:44 +0330

    [DELETE] additional comment

--- dream-journal/src/components/Add-Edit-Dream-Form/Add-Edit-Dream-Form.tsx
+++ dream-journal/src/components/Add-Edit-Dream-Form/Add-Edit-Dream-Form.tsx
@@ -39,7 +39,6 @@ export const AddEditDreamForm: React.FC<Props> = ({
     const title = formData.get("title") as string;
     const description = formData.get("description") as string;
     const date = formData.get("date") as string;
-    // const vibe = formData.get("vibe") as Vibe;
     const vibe = (formData.get("vibe") as Vibe) || "good";
 
     if (!title) {
